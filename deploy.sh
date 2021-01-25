ee6a67ccad0a6a6fda6c0d822be8789a52cfa8e3
Nicolay i <nic.ilinov@gmail.com>
2021-01-25 23:08:22 +0700

    ++

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,19 @@ set -e
 echo "Deploying application ..."
 
 (
-  ../start.sh 2 &&
+  ../start.sh 1 &&
     git fetch origin deploy &&
     git reset --hard origin/deploy &&
     npm install &&
     npm run build &&
     cp -Rf dist/req-resp-Utility ../frontend &&
     echo "Application deployed!" &&
-    ../done.sh 2
+    ../done.sh 1
 )
 
 errorCode=$?
 if [ $errorCode -ne 0 ]; then
-  echo "We have an error" && ../error.sh 2
+  echo "We have an error" && ../error.sh 1
 
   exit $errorCode
 fi
