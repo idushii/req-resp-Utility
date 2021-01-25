18f80fd6832e9de0b0953e20cf8585b500d94ea1
Nicolay i <nic.ilinov@gmail.com>
2021-01-25 15:03:14 +0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,12 @@ echo "Deploying application ..."
 # this subshell is a scope of try
 # try
 (
-  git fetch origin deploy &&  git reset --hard origin/deploy && npm install1 &&  npm run build && cp -Rf dist/req-resp-Utility  ../frontend &&  echo "Application deployed!"
+  git fetch origin deploy &&
+  git reset --hard origin/deploy &&
+  npm install &&
+  npm run build &&
+  cp -Rf dist/req-resp-Utility  ../frontend &&
+  echo "Application deployed!"
 )
 # and here we catch errors
 # catch
