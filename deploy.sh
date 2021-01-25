9f21dbe09a011e14e4a8062e777a1e30912a557f
Nicolay i <nic.ilinov@gmail.com>
2021-01-25 14:59:59 +0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,26 @@ set -e
 
 echo "Deploying application ..."
 
-git fetch origin deploy
-git reset --hard origin/deploy
+# this subshell is a scope of try
+# try
+(
+  git fetch origin deploy
+  git reset --hard origin/deploy
 
-npm install
+  npm install1
 
-npm run build
+  npm run build
 
-cp -Rf dist/req-resp-Utility  ../frontend
+  cp -Rf dist/req-resp-Utility  ../frontend
 
-echo "Application deployed!"
+  echo "Application deployed!"
+)
+# and here we catch errors
+# catch
+errorCode=$?
+if [ $errorCode -ne 0 ]; then
+  echo "We have an error"
+
+
+  exit $errorCode
+fi
